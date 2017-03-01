98f0bd3f95e8100410aaa6dc94829b8f9eeab72d
greyhoundforty <rtiffany@my.lonestar.edu>
2017-02-28 18:07:46 -0600

    Adding more colors to backup script for better readability

--- backup_script.sh
+++ backup_script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 ## v1 - everything runs as root (current version)
+overview() { 
+  echo -e "\r\033[K\e[36mThis script will install s3cmd and rsnapshot on your server and help with some basic backup configurations.\e[0m"
+  echo -e "\r\033[K\e[36mBy default rsnapshot is configured to only backup this system, but can be configured to backup remote systems as well.\e[0m"
+}
+
+overview
 
 check_your_privilege () {
     if [[ "$(id -u)" != 0 ]]; then
@@ -15,7 +21,7 @@ set_install_variables()	{
 
   # OS specific variables.
   if [ -e /etc/redhat-release ] ; then
-    echo "Red Hat based distribution detected..."
+    echo -e "\r\033[K\e[36mRed Hat based distribution detected...\e[0m"
     OS_VENDOR=$(awk '{print $1}' /etc/redhat-release | tr '[a-z]' '[A-Z]')
     if [ "${OS_VENDOR}" = "RED" ]; then
           OS_VENDOR="REDHAT"
@@ -25,16 +31,16 @@ set_install_variables()	{
     OS_INSTALL_TOOL="/usr/bin/yum -y install"
     MAJOR_VERSION=$(rpm -qa \*-release | grep -Ei "oracle|redhat|centos" | cut -d"-" -f3)
       if [ "${OS_VENDOR}" = "REDHAT" ] && [ "${MAJOR_VERSION}" = "6" ]; then
-        rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+        rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm >/dev/null 2>>install.log
       elif [ "${OS_VENDOR}" = "CENTOS" ] && [ "${MAJOR_VERSION}" = "6" ]; then
-        rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+        rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm >/dev/null 2>>install.log
       elif [ "${OS_VENDOR}" = "REDHAT" ] && [ "${MAJOR_VERSION}" = "7" ]; then
-        yum install epel-release -y
+        yum install epel-release -y >/dev/null 2>>install.log
       elif [ "${OS_VENDOR}" = "CENTOS" ] && [ "${MAJOR_VERSION}" = "7" ]; then
-        yum install epel-release -y
+        yum install epel-release -y >/dev/null 2>>install.log
       fi
   elif [ -e /usr/bin/lsb_release ] ; then
-    echo "Debian based distribution detected..."
+    echo -e "\r\033[K\e[36mDebian based distribution detected...\e[0m"
     OS_VENDOR=$(lsb_release -si | tr '[a-z]' '[A-Z]')
     OS_VERSION_MAJOR=$(lsb_release -sr | cut -d. -f1)
     OS_VERSION_MINOR=$(lsb_release -sr | cut -d. -f2)
@@ -45,7 +51,7 @@ set_install_variables()	{
 
 
 install_tools() {
-	$OS_INSTALL_TOOL s3cmd rsync rsnapshot wget 
+	$OS_INSTALL_TOOL s3cmd rsync rsnapshot wget >/dev/null 2>>install.log
 }
 
 configure_rsnapshot()
@@ -54,8 +60,9 @@ configure_rsnapshot()
   wget -O /etc/rsnapshot.conf https://gist.githubusercontent.com/greyhoundforty/6b6975d973f5550fce69b71ed8485d34/raw/4091bb17f03dc9b0a3f745b348e686db14b4027e/rsnapshotv2.conf
   echo
   echo
-  echo -n "Please supply the directory you would like to use to store your backups. Use the full path with a trailing slash (example: /backups/)  "
+  echo -n -e "\r\033[K\e[36mPlease supply the directory you would like to use to store your backups. Use the full path with a trailing slash (example: /backups/)\e[0m  "
   read RSNAPSHOT_BACKUP_DIR
+  echo "Set rsnapshot backup directory to ${RSNAPSHOT_BACKUP_DIR}"
   
   sed -i "s|BACKUP_DIR|$RSNAPSHOT_BACKUP_DIR|" /etc/rsnapshot.conf
   echo "Testing rsnapshot configuration"
@@ -67,21 +74,18 @@ configure_s3cmd() {
 	wget -O "$HOME/.s3cfg" https://gist.githubusercontent.com/greyhoundforty/676814921b8f4367fba7604e622d10f3/raw/f6ce1f2248c415cefac4eec4f1c112ad4a03a0d1/s3cfg
 	echo 
 	echo 
-	echo -en "Please supply your Cloud Object Storage (S3) Access Key:\n:: "
+	echo -n -e "\r\033[K\e[36mPlease supply your Cloud Object Storage (S3) Access Key:\e[0m  "
 	read -s COS_ACCESS_KEY
 	sed -i "s|cos_access_key|$COS_ACCESS_KEY|" "$HOME"/.s3cfg
   echo ""
-	echo -en "Please supply your Cloud Object Storage (S3) Secret Key:\n:: "
+	echo -n -e "\r\033[K\e[36mPlease supply your Cloud Object Storage (S3) Secret Key:\e[0m  "
 	read -s COS_SECRET_KEY
 	sed -i "s|cos_secret_key|$COS_SECRET_KEY|" "$HOME/.s3cfg"
 	echo 
-	echo -en "Would you like to use the Public or Private Cloud Object Storage S3 endpoint?\n:: "
+	echo -n -e "\r\033[K\e[36mPlease supply your Cloud Object Storage (S3) Endpoint:\e[0m  "
 	read ENDPOINT 
-	case "$ENDPOINT" in  
-  		public|Public) sed -i "s|cos_endpoint|s3-api.us-geo.objectstorage.softlayer.net|g" "$HOME/.s3cfg";; 
-  		private|Private) sed -i "s|cos_endpoint|s3-api.us-geo.objectstorage.service.networklayer.com|g" "$HOME/.s3cfg" ;; 
-  		*) echo "Did not set an endpoint, you will need to manually update the ~/.s3cfg config file." ;; 
-	esac
+  sed -i "s|cos_endpoint|$ENDPOINT|g" "$HOME/.s3cfg"
+  echo ""
   echo "We will now test our config file by creating a test bucket"
   s3cmd mb s3://testbckt
   if [ $(s3cmd ls | egrep 'testbckt' | wc -l) = "1" ];then
@@ -93,6 +97,16 @@ configure_s3cmd() {
   fi
   
 }
+
+post_install() {
+
+echo "Installation and configuration of rsnapshot and s3cmd has completed."
+echo "Please note that by default this script only configures rsnapshot to backup this system."
+echo "If you would like to add remote systems for rsnapshot to also backup, you will need to edit the /etc/rsnapshot.conf file."
+echo "The following guide should assist in setting up remote hosts in rsnapshot: LINK TO THING I WRITE ABOUT THIS"
+}
+
+
 
 #cos_backup_schedule() { 
 
@@ -107,3 +121,4 @@ set_install_variables
 install_tools
 configure_rsnapshot
 configure_s3cmd
+post_install
